e6f4bc816a4237ff66e6ce81ef52d21eb3b4b75d
vpopi4 <vpopi4@yandex.ru>
2024-08-12 02:55:07 +0500

    fix schema and add init query

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    mpa_ratings (name, description)
+VALUES
+    ('G', 'no age restrictions for the movie'),
+    ('PG', 'parental guidance is recommended for children'),
+    ('PG-13', 'not recommended for children under 13'),
+    ('R', 'under 17 requires accompanying parent or adult guardian'),
+    ('NC-17', 'no one 17 and under admitted');
